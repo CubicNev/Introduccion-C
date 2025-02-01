6ea2070f74a9ec2834e6f8893bfe95efe5dfb818
CubicNev <juan.nevtor@hotmail.com>
2025-01-31 23:19:20 -0600

    Add: Starting switch

---
+++ 05 Toma de decisiones/08-switch.c
@@ -0,0 +1,22 @@
+/**
+ *  Using switch-case structure
+ *  Author: Carlos Nevárez
+ *  Date: 31/01/2025
+ */
+
+#include <stdio.h>
+
+int opt1 = 0;
+
+int main()
+{
+    switch (opt1)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
+}
